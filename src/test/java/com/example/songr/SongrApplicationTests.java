087826fed4d85ee65fd7137cd5aef37be345c820
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-10-25 19:01:23 +0300

    done

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.songr;
 
+import com.example.songr.model.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class SongrApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Album album=new Album("ali","ali",4,5,"http://ali.comskdw.js");
+		assertEquals("ali",album.getTitle());
+		assertEquals("ali",album.getArtist());
+		assertEquals(5,album.getLength());
+		assertEquals(4,album.getSongCount());
 	}
 
 }
